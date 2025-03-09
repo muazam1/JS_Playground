4cf3ba9b1a49e4e53bbe904e78ecbfee88c3484b
Muazam Qayyum <126908205+muazam1@users.noreply.github.com>
2025-03-09 08:25:30 +0000

    datatypes

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//Treat your js code as newer version of js
+
+//alert(3 + 3) Error because we are using nodejs not browser.
+
+console.log(3 +
+    3);//code readability should be high
+console.log("Muazam qayyum");
+
+let name = "muazam"
+let age = 20
+let isloggedIn = true
+
+// number => 2 to power of 53 range
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//Objects 
+console.log(typeof age);//type of any variable 
+
+
+
