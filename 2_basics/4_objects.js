49a6d51d8731979d5c498316d20ef71991593a68
Muazam Qayyum <126908205+muazam1@users.noreply.github.com>
2025-03-16 09:33:26 +0000

    objects done

---
+++ 2_basics/4_objects.js
@@ -0,0 +1,55 @@
+//  singleton // declare with the help of constructor
+
+//const user = new Object()
+const user = {}
+user.id = "123abs"
+user.name = "Muazam"
+user.isLoggedIN = false
+
+console.log(user);
+console.log(Object.keys(user));// prints all the keys 
+console.log(Object.values(user)); // print all the values 
+console.log(Object.entries(user)); // make separate entry in array
+console.log(user.hasOwnProperty('isLoggedIN'));// true
+
+
+const regularUser = {
+    email : "muazam@google.com",
+    fullname : {
+        userfullname : {
+            firstname : "Muazam",
+            lastname : "Qayyum"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);//Muazam
+
+
+//combining two objects
+const obj1 = {
+    1:  "a",
+    2 : "b"
+}
+const obj2 = {
+    3:  "a",
+    4 : "b"
+}
+// Different methods like array combination 
+// const obj3 = {obj1, obj2}
+// console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2)
+obj3 = {...obj1, ...obj2}//spreading method
+console.log(obj3);
+
+
+
+// array of an object 
+const users = [
+    {
+        id : 1 , 
+        email : "m@gmail.com"
+  }
+]
+//users[1].email
+
+// Nearly all objects in JavaScript are instances of Object; a typical object inherits properties (including methods) from Object.prototype, although these properties may be shadowed (a.k.a. overridden). The only objects that don't inherit from Object.prototype are those with null prototype, or descended from other null prototype objects.
