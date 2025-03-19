cedb4f9053e96f9a74a7065f5023dc3005add5ec
Muazam Qayyum <126908205+muazam1@users.noreply.github.com>
2025-03-19 19:34:06 +0000

    started with control flow

---
+++ 4_control_flow/one.js
@@ -0,0 +1,20 @@
+// sometime we don't want the whole code to execute 
+// for that we use control flow 
+
+
+// if 
+
+// if (condition){ //if the condition is true then we will execute the code otherwise not
+// }
+const isuserLoggedIn = true
+
+// if (isuserLoggedIn){
+//     console.log("user logged in");
+// }
+
+const temp = 41
+if (temp < 50){
+    console.log("Less than 50");
+}else{
+    console.log("Greater than 50");
+}//less than 50 got executed 
