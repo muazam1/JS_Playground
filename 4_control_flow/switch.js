19368ca45cbe5e8ab448755696ec2ec0c7187943
Muazam Qayyum <126908205+muazam1@users.noreply.github.com>
2025-03-23 18:16:39 +0000

    switch statment in js

---
+++ 4_control_flow/switch.js
@@ -0,0 +1,55 @@
+// switch Statment
+// basic sytax 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;//after match the break statment break the execution
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+    case 9:
+        console.log("Sep");
+        break;
+    case 10:
+        console.log("Oct");
+        break;
+    case 11:
+        console.log("Nov");
+        break;
+    case 12:
+        console.log("Dec");
+        break;
+
+    default:
+        console.log("Default case match");  
+        break;
+} 
+
+
